2e9f75d09ea4836b4071996a4743e48992b53895
Arturo Valdez <santiguardado27@gmail.com>
2025-03-14 19:58:50 -0600

    Creacion de tabla listado de Clientes en Swing

---
+++ src/main/java/gm/zona_fit/ZonaFitSwing.java
@@ -0,0 +1,32 @@
+package gm.zona_fit;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
+import gm.zona_fit.gui.ZonaFitForma;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
+@SpringBootApplication
+public class ZonaFitSwing {
+    public static void main(String[] args) {
+        //Configuracion tema oscuro
+        FlatDarculaLaf.setup();
+
+        // Instanciar la fabrica de spring
+        ConfigurableApplicationContext contexSpring =
+                new SpringApplicationBuilder(ZonaFitSwing.class).
+                        headless(false).
+                        web(WebApplicationType.NONE).
+                        run(args);
+
+        //Crear un objeto de Swing
+        SwingUtilities.invokeLater(() -> {
+            ZonaFitForma zonaFitForma = contexSpring.getBean(ZonaFitForma.class);
+            zonaFitForma.setVisible(true);
+        });
+
+    }
+}
--- src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -10,10 +10,21 @@ import javax.swing.*;
 @Component
 public class ZonaFitForma extends JFrame{
     private JPanel panelPrincipal;
+    private JTable clientesTabla;
     IClienteServicio clienteServicio;
 
     @Autowired
     public ZonaFitForma(ClienteServicio clienteServicio){
         this.clienteServicio = clienteServicio;
+        iniciarForma();
     }
+
+    private void iniciarForma() {
+        setContentPane(panelPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900, 700);
+        setLocationRelativeTo(null);//Centra Ventana
+    }
+
+
 }
